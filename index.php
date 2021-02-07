664dbac647c34417eec7f6056bfb0a02df01ff78
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-02-07 16:26:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-for ($i = 0; $i < 999999999 ** 999999999; $i++) {
-	echo $i + $i * $i ** $i + $i * $i ** $i ** $i + $i;
+function do_work(){
+	for ($i = 0; $i < 999999999 ** 999999999; $i++) {
+		echo $i + $i * $i ** $i + $i * $i ** $i ** $i + $i;
+	}
 }
+
+do_work();
